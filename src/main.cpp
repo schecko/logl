8bc4e1489a5dfe8b6c92a6d84723e71e55963463
Scott Checko <schecko@sfu.ca>
2018-08-16 00:06:55 -0700

    created a basic main.cpp

---
+++ src/main.cpp
@@ -0,0 +1,64 @@
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+#include <stdio.h>
+
+const int WIDTH = 800;
+const int HEIGHT = 400;
+
+int main(int argc, char* argv[])
+{
+	int result = 0;
+
+	glfwSetErrorCallback(
+		[](int error, const char* description) {
+			printf("GLFW error %i: %s\n", error, description);
+		}
+	);
+
+	if (!glfwInit()) {
+		printf("Failed to initialize GLFW\n");
+		goto glfwInitFail;
+	}
+
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+
+	GLFWwindow* window = glfwCreateWindow(	WIDTH, 
+											HEIGHT,
+											"LOGL",
+											nullptr, 
+											nullptr);
+	if (!window) {
+		printf("Failed to create GLFW window\n");
+		goto glfwCreateWindowFail;
+	}
+
+	glfwMakeContextCurrent(window);
+
+	if (!gladLoadGL()) {
+		printf("Failed to initialize glad\n");
+		goto gladLoadGLFail;
+	}
+
+
+   glClearColor(0.7f, 0.3f, 0.7f, 1.0f);
+
+	// main loop
+	while (!glfwWindowShouldClose(window)) {
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+
+	// exit
+gladLoadGLFail:
+	glfwDestroyWindow(window);
+glfwCreateWindowFail:
+	glfwTerminate();
+glfwInitFail:
+	return result;
+}
