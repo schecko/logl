89d4a6acca31ec04f58b43d2a1a31915ce177a28
Scott Checko <schecko@sfu.ca>
2018-08-23 23:50:46 -0700

    added blending

--- src/textures/main.cpp
+++ src/textures/main.cpp
@@ -91,9 +91,37 @@ struct Pipeline {
 	}
 };
 
+struct Texture {
+	u32 id;
 
+	Texture(const char* file, u32 format) {
+		glGenTextures(1, &this->id);
+		glBindTexture(GL_TEXTURE_2D, this->id);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 
-int helloTextures(GLFWwindow* window) {
+		int width, height, channels;
+		char imagePath[255];
+		sprintf_s(imagePath, "%s%s", DATA_DIR, file);
+
+		u32 desiredChannel = 3;
+		if (format == GL_RGBA) {
+			desiredChannel = 4;
+		}
+
+		unsigned char* data = stbi_load(imagePath, &width, &height, &channels, desiredChannel);
+
+		ASSERT(data);
+
+		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, format, GL_UNSIGNED_BYTE, data);
+		glGenerateMipmap(GL_TEXTURE_2D);
+		stbi_image_free(data);
+	}
+};
+
+int singleTexture(GLFWwindow* window) {
 
 	glClearColor(0.7f, 0.3f, 0.7f, 1.0f);
 
@@ -177,6 +205,7 @@ int helloTextures(GLFWwindow* window) {
 	// setup texture state
 	u32 texture;
 	glGenTextures(1, &texture);
+	glActiveTexture(GL_TEXTURE0);
 	glBindTexture(GL_TEXTURE_2D, texture);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
@@ -184,7 +213,7 @@ int helloTextures(GLFWwindow* window) {
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 
 	int width, height, channels;
-	char imageName[] = "/gears.jpeg";
+	char imageName[] = "/wall.jpg";
 	char imagePath[sizeof(DATA_DIR) + sizeof(imageName)];
 	sprintf_s(imagePath, "%s%s", DATA_DIR, imageName);
 	unsigned char* data = stbi_load(imagePath, &width, &height, &channels, 0);
@@ -198,9 +227,10 @@ int helloTextures(GLFWwindow* window) {
 	// main loop
 	while (!glfwWindowShouldClose(window)) {
 		glClear(GL_COLOR_BUFFER_BIT);
-		
+
 		pipeline.use();
 		glBindVertexArray(vao);
+		glActiveTexture(GL_TEXTURE0);
 		glBindTexture(GL_TEXTURE_2D, texture);
 
 		float time = (float)glfwGetTime();
@@ -216,9 +246,134 @@ int helloTextures(GLFWwindow* window) {
 	return 0;
 }
 
+int blendedTextures(GLFWwindow* window) {
+
+	glClearColor(0.7f, 0.3f, 0.7f, 1.0f);
+
+	float vertices[] = {
+		// positions			// colors           // texture coords
+		0.5f,  0.5f, 0.0f,		1.0f, 0.0f, 0.0f,   1.0f, 1.0f,   // top right
+		0.5f, -0.5f, 0.0f,		0.0f, 1.0f, 0.0f,   1.0f, 0.0f,   // bottom right
+		-0.5f, -0.5f, 0.0f,		0.0f, 0.0f, 1.0f,   0.0f, 0.0f,   // bottom left
+		-0.5f,  0.5f, 0.0f,		1.0f, 1.0f, 0.0f,   0.0f, 1.0f    // top left 
+	};
+
+	u32 indices[] = {
+		0, 1, 3,
+		1, 2, 3
+	};
+
+	//vertex array object
+	u32 vao;
+	glGenVertexArrays(1, &vao);
+	glBindVertexArray(vao);
+
+	// vertex buffer
+	u32 vbo;
+	glGenBuffers(1, &vbo);
+	glBindBuffer(GL_ARRAY_BUFFER, vbo);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	u32 ebo;
+	glGenBuffers(1, &ebo);
+
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+	// vertex shader
+	const char* vertexShaderSource = R"(
+		#version 330 core
+		layout (location = 0) in vec3 pos;
+		layout (location = 1) in vec4 color;
+		layout (location = 2) in vec2 texCoords;
+
+		out vec4 vColor;
+		out vec2 vCoord;
+
+		void main() 
+		{
+			gl_Position = vec4(pos.xyz, 1.0);
+			vColor = color;
+			vCoord = texCoords;
+		}	
+	)";
+
+	// fragment shader
+	const char* fragmentShaderSource = R"(
+		#version 330 core
+
+		in vec4 vColor;
+		in vec2 vCoord;
+		uniform sampler2D uTexture1;
+		uniform sampler2D uTexture2;
+		out vec4 fColor;
+
+		void main() 
+		{
+			vec4 c1 = texture(uTexture1, vCoord);
+			vec4 c2 = texture(uTexture2, vCoord);
+			
+			vec3 color = c2.rgb;
+			if(c1.a > 0.1) {
+				color = mix(c1, c2, .5).rgb;
+			}
+
+			fColor = vec4(color, 1.0);
+		}
+	)";
+
+	Pipeline pipeline = Pipeline(vertexShaderSource, fragmentShaderSource);
+
+	if (!pipeline.id) {
+		return -5;
+	}
+
+	// setup vertex attributes
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), nullptr);
+	glEnableVertexAttribArray(0);
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(3 * sizeof(float)));
+	glEnableVertexAttribArray(1);
+	glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(6 * sizeof(float)));
+	glEnableVertexAttribArray(2);
+
+
+
+	Texture tex0 = Texture("/face.png", GL_RGBA);
+	Texture tex1 = Texture("/wall.jpg", GL_RGBA);
+
+	// main loop
+	while (!glfwWindowShouldClose(window)) {
+		glClear(GL_COLOR_BUFFER_BIT);
+		
+		pipeline.use();
+		pipeline.setUniform("uTexture1", 0);
+		pipeline.setUniform("uTexture2", 1);
+
+		glBindVertexArray(vao);
+		glActiveTexture(GL_TEXTURE0);
+		glBindTexture(GL_TEXTURE_2D, tex0.id);
+		glActiveTexture(GL_TEXTURE1);
+		glBindTexture(GL_TEXTURE_2D, tex1.id);
+
+		float time = (float)glfwGetTime();
+		float color = (float)sin(time) / 2.0f + 0.5f;
+
+		glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+
+		//draw
+		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+
+	return 0;
+}
+
 int main(int argc, char* argv[])
 {
 	int result = 0;
+	stbi_set_flip_vertically_on_load(true);
 
 	glfwSetErrorCallback(
 		[](int error, const char* description) {
@@ -259,8 +414,11 @@ int main(int argc, char* argv[])
 
 	// turn on the different render outputs by changing the falses to 
 	// true. very primitive but quick to prototype.
-	if (true) {
-		result = helloTextures(window);
+	if (false) {
+		result = singleTexture(window);
+	}
+	else if (true) {
+		result = blendedTextures(window);
 	}
 	else {
 		result = 0;
