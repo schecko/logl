5a49fa60e0a399923f9f33c55e22a1e0832fc8a4
Scott Checko <scott.checko@hotmail.com>
2019-04-14 23:35:46 -0700

    loading images

--- src/hello_world.cpp
+++ src/hello_world.cpp
@@ -2,24 +2,49 @@
 #include <stdio.h>
 #include <SDL2/SDL.h>
 
+
+SDL_Surface* G_screenSurface = nullptr;
+SDL_Window* G_screenWindow = nullptr;
+
+SDL_Surface* loadImage(char* path) {
+	SDL_Surface* result = SDL_LoadBMP(path);
+	if(result) {
+		SDL_Surface* converted = SDL_ConvertSurface(result, G_screenSurface->format, 0);
+		if(converted) {
+			SDL_FreeSurface(result);
+			result = converted;
+		}
+	}
+	return result;
+}
+
 int main(int argc, char** argv) {
 	if(SDL_Init(SDL_INIT_VIDEO)) {
 		printf("sdl error: %s\n", SDL_GetError());
 		return EXIT_FAILURE;
 	}
 
-	SDL_Window* window = SDL_CreateWindow(
+	G_screenWindow = SDL_CreateWindow(
 		"Bleh",
 		SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
 		620, 387,
 		SDL_WINDOW_OPENGL
 	);
-	if(!window) {
+	if(!G_screenWindow) {
 		printf("sdl create window error: %s\n", SDL_GetError());
 		return EXIT_FAILURE;
 	}
 
-	SDL_Surface* surface = SDL_GetWindowSurface(window);
+	G_screenSurface = SDL_GetWindowSurface(G_screenWindow);
+
+	SDL_Surface* randomImage = loadImage("data/face.png");
+
+	SDL_Rect blitDestination = {};
+	blitDestination.w = 100;
+	blitDestination.h = 100;
+	SDL_BlitScaled(randomImage, 0, G_screenSurface, &blitDestination);
+
+	SDL_Delay(2000);
 
 	bool quit = false;
 	SDL_Event e;
@@ -58,11 +83,11 @@ int main(int argc, char** argv) {
 
 		}
 
-		SDL_FillRect(surface, NULL, SDL_MapRGB(surface->format, r, g, b));
-		SDL_UpdateWindowSurface(window);
+		//SDL_FillRect(G_screenSurface, NULL, SDL_MapRGB(G_screenSurface->format, r, g, b));
+		SDL_UpdateWindowSurface(G_screenWindow);
 	}
 
-	SDL_DestroyWindow(window);
+	SDL_DestroyWindow(G_screenWindow);
 
 	SDL_Quit();
 
