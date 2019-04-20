af8f86703532db23c0daed3627043900e18f3c0a
Scott Checko <scott.checko@hotmail.com>
2019-04-19 22:06:17 -0700

    added sdl_image, modifying makefile

--- Makefile
+++ Makefile
@@ -1,9 +1,18 @@
-OBJS = src/*
-OUT = hello_world.out
+SRCS = $(wildcard src/*.cpp) ./lib/glad/src/glad.c
+OBJS = $(SRCS:.c*=.o)
+DEPS = $(OBJS:.o=.d)
+INC = -Ilib/glad/include
+OUT = main
 CC = g++
-COMPILER_FLAGS = -w -Wall -pedantic `sdl2-config --cflags --libs`
-LINKER_FLAGS = -lSDL2
+COMPILER_FLAGS = -ggdb -Wall -pedantic -c `sdl2-config --cflags`
+LINKER_FLAGS = -lSDL2 -lSDL2_image `sdl2-config --libs`
+
+%.o: %.c
+	$(CC) $(INC) $< $(COMPILER_FLAGS) $(OUTPUT_OPTION) $(@:.d=.o) >$@
 
 all: $(OBJS)
-	$(CC) $(OBJS) $(COMPILER_FLAGS) $(LINKER_FLAGS) -o ${OUT}
+	$(CC) $(INC) $(OBJS) $(LINKER_FLAGS) -o ${OUT}
+
+
+run: all
 	./$(OUT)
--- src/hello_world.cpp
+++ src/hello_world.cpp
@@ -1,13 +1,16 @@
 
-#include <stdio.h>
 #include <SDL2/SDL.h>
-
+#include <SDL2/SDL_image.h>
+#include <stdio.h>
+#include "glad/glad.h"
+#include <SDL2/SDL_opengl.h>
 
 SDL_Surface* G_screenSurface = nullptr;
 SDL_Window* G_screenWindow = nullptr;
 
-SDL_Surface* loadImage(char* path) {
-	SDL_Surface* result = SDL_LoadBMP(path);
+SDL_Surface* loadImage(const char* path) {
+	SDL_assert_paranoid(G_screenSurface);
+	SDL_Surface* result = IMG_Load(path);
 	if(result) {
 		SDL_Surface* converted = SDL_ConvertSurface(result, G_screenSurface->format, 0);
 		if(converted) {
@@ -19,10 +22,15 @@ SDL_Surface* loadImage(char* path) {
 }
 
 int main(int argc, char** argv) {
+
 	if(SDL_Init(SDL_INIT_VIDEO)) {
 		printf("sdl error: %s\n", SDL_GetError());
 		return EXIT_FAILURE;
 	}
+
+	int image_load = IMG_Init(IMG_INIT_JPG | IMG_INIT_PNG);
+	SDL_assert((image_load & IMG_INIT_JPG) && (image_load & IMG_INIT_PNG));
+
 
 	G_screenWindow = SDL_CreateWindow(
 		"Bleh",
@@ -37,47 +45,44 @@ int main(int argc, char** argv) {
 
 	G_screenSurface = SDL_GetWindowSurface(G_screenWindow);
 
-	SDL_Surface* randomImage = loadImage("data/face.png");
+	SDL_Surface* randomImage = loadImage("./data/face.png");
 
 	SDL_Rect blitDestination = {};
-	blitDestination.w = 100;
-	blitDestination.h = 100;
+	blitDestination.w = G_screenSurface->w;
+	blitDestination.h = G_screenSurface->h;
 	SDL_BlitScaled(randomImage, 0, G_screenSurface, &blitDestination);
 
-	SDL_Delay(2000);
+	{ // gl init
+		SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
+		SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 1);
+		SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
+
+		SDL_GLContext gContext = SDL_GL_CreateContext(G_screenWindow);
+		SDL_assert_release(gContext);
+
+		gladLoadGLLoader(SDL_GL_GetProcAddress);
+		// enable vsync
+		SDL_GL_SetSwapInterval(1);
+
+	}
+
 
 	bool quit = false;
 	SDL_Event e;
-	int time = 0;
-	int r = 0, g = 0, b = 0;
 	while(!quit) {
-		time++;
 
 		while(SDL_PollEvent(&e)) {
 			if(e.type == SDL_QUIT) quit = true;
 			switch(e.key.keysym.sym) {
 				case SDLK_UP:
-					r = 30;
-					g = 0;
-					b = 255;
 					break;
 				case SDLK_DOWN:
-					r = 255;
-					g = 0;
-					b = 255;
 					break;
 				case SDLK_LEFT:
-					r = 0;
-					g = 255;
-					b = 0;
 					break;
 				case SDLK_RIGHT:
-					r = 255;
-					g = 0;
-					b = 0;
 					break;
 				default:
-					r = g = b = time & 0xFF;
 					break;
 			}
 
@@ -89,6 +94,7 @@ int main(int argc, char** argv) {
 
 	SDL_DestroyWindow(G_screenWindow);
 
+	IMG_Quit();
 	SDL_Quit();
 
 	printf("success");
